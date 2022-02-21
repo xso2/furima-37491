3354909add931a546cdb0e9c52ad96de106a5b2f
xso2 <odaken1002@gmail.com>
2022-02-21 17:17:53 +0900

    商品情報編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show] 
+  before_action :authenticate_user!, except: [:index, :show, ] 
   before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
+
 
   def index
     @items  = Item.includes(:user).order('created_at DESC')
@@ -17,12 +19,22 @@ class ItemsController < ApplicationController
     else
       render :new
     end
+  end
 
   def show
   end
 
-
+  def edit
   end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
 
 
 
@@ -48,7 +60,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to root_path if @item.user != current_user || @item.purchase_record.present?
+    redirect_to root_path if @item.user != current_user #|| @item.purchase_record.present?
+    end
   end
-  
-end
+
