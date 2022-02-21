950c96975afb5d495f7b9a3c139b035a4c6a3028
xso2 <odaken1002@gmail.com>
2022-02-21 18:07:33 +0900

    商品削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show] 
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
     @items  = Item.includes(:user).order('created_at DESC')
@@ -18,22 +20,45 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+  end
 
+  def edit
+  end
 
+  def update
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
 
   private
 
   def item_params
     params.require(:item).permit(
-      :image,
-      :name,
-      :introduction,
-      :category_id,
+      :name, 
+      :introduction, 
+      :category_id, 
+      :item_condition_id, 
+      :postage_payer_id, 
+      :prefecture_id, 
+      :preparation_day, 
       :price,
-      :item_condition_id,
-      :postage_payer_id,
-      :prefecture_id,
-      :preparation_day_id
-    ).merge(user_id: current_user.id)
+      :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to root_path if @item.user != current_user #|| @item.purchase_record.present?
   end
 end
