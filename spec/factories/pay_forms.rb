df961abd2d76cd55a21fac25b84133031e97181e
xso2 <odaken1002@gmail.com>
2022-02-25 14:24:44 +0900

    テストコード修正

--- spec/factories/pay_forms.rb
+++ spec/factories/pay_forms.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     post_code        { '123-4567' }
     prefecture_id    { Faker::Number.between(from: 2, to: 48) }
     city             { Faker::Address.city }
-    address          { Faker::Address.street_address }
+    house_number          { Faker::Address.street_address }
     building_name    { Faker::Address.building_number }
     phone_number     { Faker::Number.leading_zero_number(digits: 10) }
     token            { 'tok_abcdefghijk00000000000000000' }
